ae623db25d15400bcb61acdb7d35f8baee3fb8a9
Christian Eichelmann <christian.eichelmann@1und1.de>
2014-06-07 12:53:37 +0200

    added script for jenkins

---
+++ makepkg.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#
+# This file is used to build a debain package in jenkins
+#
+
+# create temporary directory
+TMPDIR=`mktemp -d -u`
+
+# adjust changelog
+DISTRIBUTION=$1
+VERSION=`./rebootd.py --version`
+debchange --newversion ${VERSION} --distribution ${DISTRIBUTION} --force-distribution -m "released version ${VERSION} for ${DISTRIBUTION}"
+
+# create build environment
+mkdir -p deb
+
+# copy workspace to deb/workspace
+cp -a ${WORKSPACE} ${TMPDIR}
+mv ${TMPDIR} deb/workspace
+
+# build package in build dir
+cd deb/workspace
+debuild -i -us -uc -b
+
+# go pack to workspace
+cd ${WORKSPACE}
