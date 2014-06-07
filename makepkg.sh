6dff4c0efac086e75bd3884354119d36f11cb2f0
Christian Eichelmann <christian.eichelmann@1und1.de>
2014-06-07 13:04:04 +0200

    debugging

--- makepkg.sh
+++ makepkg.sh
@@ -10,6 +10,7 @@ TMPDIR=`mktemp -d -u`
 # adjust changelog
 DISTRIBUTION=$1
 VERSION=`./rebootd.py --version`
+echo $VERSION
 debchange --newversion ${VERSION} --distribution ${DISTRIBUTION} --force-distribution -m "released version ${VERSION} for ${DISTRIBUTION}"
 
 # create build environment
